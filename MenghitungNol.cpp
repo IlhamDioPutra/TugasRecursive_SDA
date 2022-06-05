a699f0b154e0cb5e629fd3bcbcd39765c19506fa
IlhamDioPutra <99026821+IlhamDioPutra@users.noreply.github.com>
2022-06-05 22:45:42 +0700

    Add files via upload

---
+++ MenghitungNol.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+//nama :Ilham Dio Putra
+//npm : G1A021024
+using namespace std;
+int zero(int angka)
+{
+static int hitung=0;
+    if(angka>0)
+    {
+
+        if(angka%10==0)
+        hitung++;
+
+        zero(angka/10);
+    }
+    return hitung;
+}
+int main()
+{
+    int n;
+    cout<<"---------Menghitung Jumlah Angka Nol---------"<<endl;
+    cout<<"---------------------------------------------"<<endl;
+    cout<<"Masukkan angka :";
+    cin>>n;
+    cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+    cout<<"Banyak angka nol : "<<zero(n);
+}
